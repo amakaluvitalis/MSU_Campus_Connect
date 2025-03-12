cbfcb41242fc011067bc26a66778a7a708c46114
prwtyhwling <omondijohnson57@gmail.com>
2025-03-12 16:23:50 +0300

    Bootrspa

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/home";
 import Login from "./pages/login";
-import Navbar from "./components/Navbar";
+// import Navbar from "./components/Navbar";
 import "./index.css";
 
 function App() {
   return (
     <Router>
-      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
